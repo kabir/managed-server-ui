5ac7fa6931225176222e09027523501aecfe3e85
Kabir Khan <kkhan@redhat.com>
2022-10-18 15:14:33 +0100

    First Hibernate Panache query tried

--- backend/src/main/java/org/wildfly/managed/app/Applications.java
+++
@@ -1,7 +0,0 @@
-package org.wildfly.managed.app;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class Applications {
-}
--- backend/src/main/java/org/wildfly/managed/model/Application.java
+++ backend/src/main/java/org/wildfly/managed/model/Application.java
@@ -2,10 +2,49 @@ package org.wildfly.managed.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity
 public class Application extends PanacheEntity {
+    @Column(unique = true)
     String name;
-    ApplicationState state;
+    ApplicationState state = ApplicationState.INITIAL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ApplicationState getState() {
+        return state;
+    }
+
+    public void setState(ApplicationState state) {
+        this.state = state;
+    }
+
+    static Application create(String name, ApplicationState state) {
+        Application app = new Application();
+        app.name = name;
+        app.state = state;
+        persist(app);
+        return app;
+    }
+
+    static Application findByName(String name) {
+        return find("name", name).firstResult();
+    }
+
+    static void delete(String name) {
+        delete("name", name);
+    }
+
+    static List<Application> all() {
+        return listAll();
+    }
 }
